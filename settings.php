6bb97d5766d57b1f3d0bfc5cf9c2ff9b0c19463c
m00min <github47@diturner.co.uk>
2013-08-23 10:57:40 +0100

    Added comment.

--- settings.php
+++ settings.php
@@ -3,7 +3,7 @@
 	// Evernote account email address.
 	$en_email = '';
 	
-	// Evernote project to save articles to.
+	// Evernote project to save articles to, no need to put the @.
 	$en_project = '';
 	
 	// Salt, used to prevent other people from sending stuff to your Evernote account.
